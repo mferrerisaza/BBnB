50555866fa8327211f6572c660d0d29d95cc0312
Miguel Ferrer <miguef7@gmail.com>
2018-03-06 11:00:58 +0100

    Fix booked_boats relation on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   has_one :profile
   has_many :boats, dependent: :destroy
   has_many :bookings
-  has_many :boats, through: :bookings, as: :booked_boats
+  has_many :booked_boats, through: :bookings, source: :boat
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
