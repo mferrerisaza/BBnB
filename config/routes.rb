545de857ace0f5b706e9fe0b01d1850204a665f3
Eric Haagensen <haagenseneric@yahoo.com>
2018-03-06 17:32:20 +0100

    nothing to see here

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   resources :boats, only: [:index, :show, :new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
