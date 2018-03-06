632773b24ae0821f2fcc18223bd57b7ca3bcd9d2
Victor Gardrinier <v.gardrinier@gmail.com>
2018-03-06 13:07:42 +0100

    make the boat_params method private

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,30 +1,32 @@
 class BoatsController < ApplicationController
 
-def new
-  @boat = Boat.new
-end
-
-def create
-  @boat = Boat.new(boat_params)
-  @boat.user = current_user
-  if @boat.save
-    redirect_to root_path
-  else
-    render 'new'
+  def new
+    @boat = Boat.new
   end
-end
 
-def edit
-  @boat = current_user.boat
-end
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    if @boat.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 
-def update
-  @boat = current_user.boat
-  boat.update(boat_params)
-  redirect_to boat_path(@boat)
-end
+  def edit
+    @boat = current_user.boat
+  end
 
-def boat_params
-  params.require(:boat).permit(:make, :year, :model, :capacity,:location, :boat_type, :price, :photo)
-end
+  def update
+    @boat = current_user.boat
+    boat.update(boat_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:make, :year, :model, :capacity,:location, :boat_type, :price, :photo)
+  end
 end
