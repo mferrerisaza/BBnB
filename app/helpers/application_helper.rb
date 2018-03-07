36e5647e8fb490148f72dc3059c082bbcca25f5a
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-07 15:29:16 +0100

    indent line in application helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def photo_or_generic(record)
     if record.photo.blank?
       if record.class == Boat
-       cl_image_path "http://res.cloudinary.com/dnf96fubu/image/upload/v1520418575/cqsfdsswkh7ebihretfo.jpg"
+        cl_image_path "http://res.cloudinary.com/dnf96fubu/image/upload/v1520418575/cqsfdsswkh7ebihretfo.jpg"
       elsif record.class == Profile
         cl_image_tag "http://res.cloudinary.com/dnf96fubu/image/upload/v1520418809/facebook-profile-picture-no-pic-avatar.jpg", class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
       end
