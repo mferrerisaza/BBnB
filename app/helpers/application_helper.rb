8bd3c755aec2ae0f6c7116571f23e99efafb2eaf
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-07 11:37:27 +0100

    add application helper mehtod for image display in card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def photo_or_generic(record)
+    if record.photo.blank?
+      cl_image_path "http://res.cloudinary.com/dnf96fubu/image/upload/v1520418575/cqsfdsswkh7ebihretfo.jpg"
+    else
+      cl_image_path record.photo, height: 300, width: 300, crop: :fill
+    end
+  end
 end
