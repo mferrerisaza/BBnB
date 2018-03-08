e5c47838e4c4f369d6049a5dc9f70b826588c615
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-08 11:47:05 +0100

    change root route to pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard', to: 'dashboards#show', as: "dashboard"
   devise_for :users
-  root to: 'boats#index'
+  root to: 'pages#home'
 
   resources :boats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
